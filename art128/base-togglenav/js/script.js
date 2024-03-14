fce7867a0ecc7853c04947a6de96cf3541d82f55
emiyaji <emiyaji@hawaii.edu>
2024-03-13 21:25:55 -1000

    added open and close to menu button

--- art128/base-togglenav/js/script.js
+++ art128/base-togglenav/js/script.js
@@ -1,2 +1,14 @@
-console.log("Yo! I'm writing JS!");
-//alert("Hello World!");
+document.addEventListener("DOMContentLoaded", function() {
+  
+    //TOGGLE MENU FOR SMALL SCREENS + DATA ATTRIBUTES not toggle//
+    const menubutton = document.querySelector('.menu-button');
+    const menunav = document.querySelector('.toggle-nav'); 
+    menubutton.addEventListener('click', function() {
+      if (menunav.getAttribute('data-navstate') === 'open') {
+          menunav.setAttribute('data-navstate', 'closed');
+      } else {
+          menunav.setAttribute('data-navstate', 'open');
+      }
+    });
+  
+  });
